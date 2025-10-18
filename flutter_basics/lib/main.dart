036df122827cca17ef9fc9d0e9d80b8fb0d466e7
SaifUllahWebDev <saifullahrao.dev@gmail.com>
2025-10-18 09:18:44 +0500

    flutter project setup

--- flutter_basics/lib/main.dart
+++ flutter_basics/lib/main.dart
@@ -8,24 +8,36 @@ runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget{
-  MyApp({super.key});
+  const MyApp({super.key});
    @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title:'My App',
 
-      home: Scaffold(
+      home: 
+    );
+  }
+
+
+}
+
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
           title: Text("MyApp", style: TextStyle(
             fontWeight: FontWeight.bold,
             color: Colors.lightBlue,
             fontSize: 25,
-          ),),
+          ),
+          ),
+          centerTitle: true,
          ),
-      )
+      );
 
-    );
   }
-
-
 }
